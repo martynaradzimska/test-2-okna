07c1a774ade72c7b4faaae74b30727158121c9b8
martynaradzimska <martyna_radzimska@hotmail.com>
2015-05-31 23:43:31 +0200

    Końcowy commit-ostatnia wercja projektu

--- WindowsFormApplication1/Form2.h
+++ WindowsFormApplication1/Form2.h
@@ -35,8 +35,15 @@ namespace WindowsFormApplication1 {
 			}
 		}
 	private: System::Windows::Forms::Button^  button1;
-	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::TextBox^  textBox1;
+	public: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Button^  button2;
+	public:
+
+	public:
+	private:
+
+
 	protected:
 
 	private:
@@ -54,42 +61,55 @@ namespace WindowsFormApplication1 {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(102, 174);
+			this->button1->Location = System::Drawing::Point(97, 171);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"SprawdŸ";
+			this->button1->Text = L"WyjdŸ";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &Form2::button1_Click);
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(42, 51);
+			this->label1->Location = System::Drawing::Point(112, 104);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(124, 13);
+			this->label1->Size = System::Drawing::Size(38, 13);
 			this->label1->TabIndex = 1;
-			this->label1->Text = L"Ile jest danego artyku³u\?";
+			this->label1->Text = L"bla bla";
 			// 
-			// textBox1
+			// label2
 			// 
-			this->textBox1->Location = System::Drawing::Point(45, 90);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
-			this->textBox1->TabIndex = 2;
-			this->textBox1->TextChanged += gcnew System::EventHandler(this, &Form2::textBox1_TextChanged);
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(90, 79);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(82, 13);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"Iloœæ artyku³ów:";
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(74, 142);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(120, 23);
+			this->button2->TabIndex = 3;
+			this->button2->Text = L"Wróæ do magazynu";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Form2::button2_Click);
 			// 
 			// Form2
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 262);
-			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Name = L"Form2";
@@ -100,21 +120,12 @@ namespace WindowsFormApplication1 {
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
-		if (button1){
-			String ^ cos = textBox1->Text;
-			MessageBox::Show("Stan artyku³u w magazynie:" + cos);
-		}
+		Application::Exit();
 	}
 	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
-		if (textBox1){
-			System::Windows::Forms::DialogResult button1 = MessageBox::Show("Dotowaruj dany artyku³!", "UWAGA!", MessageBoxButtons::OKCancel, MessageBoxIcon::Information);
-			if (button1 == ::System::Windows::Forms::DialogResult::OK){
-				Close();
-			}
-			if (button1 == ::System::Windows::Forms::DialogResult::Cancel){
-				Application::Exit();
-			}
-		}
 	}
-	};
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+		Close();
+	}
+};
 }
